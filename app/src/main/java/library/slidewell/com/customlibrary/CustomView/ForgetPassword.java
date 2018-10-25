d162db4082c088c42cd54841a5c8178fa943598b
thesurajpagare <surajmpagare75@gmail.com>
2018-10-25 12:34:14 +0530

    login

---
+++ app/src/main/java/library/slidewell/com/customlibrary/CustomView/ForgetPassword.java
@@ -0,0 +1,103 @@
+package library.slidewell.com.customlibrary.CustomView;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.CardView;
+import android.text.TextUtils;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import library.slidewell.com.customlibrary.R;
+
+/**
+ * Created by Snehal Shewale,Nashik on 25/10/18 , 9:02 AM.
+ */
+
+public class ForgetPassword extends AppCompatActivity
+{
+    EditText userEmail;
+    CardView submitUser;
+    String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
+    String getEmail,post;
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.forget_password);
+
+        userEmail = (EditText) findViewById(R.id.tvuserEmail);
+        submitUser = (CardView) findViewById(R.id.SubmitUser);
+        getEmail = userEmail.getText().toString();
+
+
+        submitUser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                Toast.makeText(getApplicationContext(),"hii submit email",Toast.LENGTH_SHORT).show();
+                userEmail.setError(null);
+                if(userEmail.getText().toString().length()==0)
+                {
+                    userEmail.setError("Empty Email");
+                }
+                else if(!isValidEmail(userEmail.getText().toString()))
+                {
+                    userEmail.setError("Invalid Email");
+                }
+
+
+
+            }
+        });
+    }
+
+    private boolean isValidEmail(CharSequence email)
+    {
+        if (!TextUtils.isEmpty(email))
+        {
+            return Patterns.EMAIL_ADDRESS.matcher(email).matches();
+        }
+        return false;
+    }
+
+//    public void setEmail(String emailValue)
+//    {
+//        Toast.makeText(getApplicationContext(),"hii got email",Toast.LENGTH_SHORT).show();
+//
+//    }
+
+
+//    public void validateEmail()
+//    {
+//            userEmail.addTextChangedListener(new TextWatcher() {
+//                @Override
+//                public void beforeTextChanged(CharSequence s, int start, int count, int after)
+//                {
+//
+//                }
+//
+//                @Override
+//                public void onTextChanged(CharSequence s, int start, int before, int count)
+//                {
+//
+//                }
+//
+//                @Override
+//                public void afterTextChanged(Editable s)
+//                {
+//                    if (getEmail.matches(emailPattern) && s.length() > 0)
+//                    {
+//                        Toast.makeText(getApplicationContext(),"valid email address",Toast.LENGTH_SHORT).show();
+//                    }
+//                    else
+//                    {
+//                        Toast.makeText(getApplicationContext(),"Invalid email address",Toast.LENGTH_SHORT).show();
+//                    }
+//                }
+//
+//            });
+//    }
+}
--- app/src/main/java/library/slidewell/com/customlibrary/CustomView/LoginActivity.java
+++ app/src/main/java/library/slidewell/com/customlibrary/CustomView/LoginActivity.java
@@ -1,10 +1,12 @@
 package library.slidewell.com.customlibrary.CustomView;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import library.slidewell.com.customlibrary.R;
@@ -12,6 +14,8 @@ import library.slidewell.com.customlibrary.R;
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener{
     EditText e1,e2;
     Button btnLogin;
+    TextView forgetPassword;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +23,17 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         e1=(EditText)findViewById(R.id.edusername);
         e2=(EditText)findViewById(R.id.edpassword);
         btnLogin=(Button)findViewById(R.id.btnLogin);
+        forgetPassword = (TextView)findViewById(R.id.ForgetPassword);
+
+        forgetPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(getApplicationContext(),ForgetPassword.class);
+                startActivity(intent);
+            }
+        });
+
 
     }
 
