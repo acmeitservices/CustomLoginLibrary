3767d76a345f2d8a5c14c88fbba24aecb9b20715
thesurajpagare <surajmpagare75@gmail.com>
2018-10-25 16:47:51 +0530

    login

--- mylibrary/src/main/java/library/slidewell/com/mylibrary/ForgetPassword.java
+++ mylibrary/src/main/java/library/slidewell/com/mylibrary/ForgetPassword.java
@@ -50,9 +50,6 @@ public class ForgetPassword extends AppCompatActivity
                 {
                     userEmail.setError("Invalid Email");
                 }
-
-
-
             }
         });
     }
